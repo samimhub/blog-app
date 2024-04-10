101d478e0bf541d95d0fa418e526fb79004b506f
samim <aktarsamim529@gmail.com>
2024-04-10 05:56:13 +0530

    new commite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
       
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin(),],
 };
